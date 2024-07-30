1c21eb72891e62e42d33976a2a9795741e36b354
AvinDevps <avinashbandela73@gmail.com>
2024-07-30 21:10:56 +0530

    Installing mysql server

--- 01-db-mysql-server.sh
+++ 01-db-mysql-server.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Enabling mysql service"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysql service"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "Setting password for root"
 
 # mysql -h 172.31.36.19 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
